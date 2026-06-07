3c46814bb3a071fe313df4545cd8acfe355468f1
John kin Betchido <johnkingbetchido03@gmail.com>
2026-06-07 10:20:15 +0800

    fix api base url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
     res.send("BANCO SERVER VERSION 999");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 || 8080;
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
